9fdeb0b52556af0a14905539370ad79ab777b7e4
RKostyukov <roma1997sport@mail.ru>
2023-04-30 15:41:47 +0300

    First commit

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+
+@Module({
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot('mongodb+srv://roman:roma25003@cluster0.v8vwp3y.mongodb.net/products?retryWrites=true&w=majority') 
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
